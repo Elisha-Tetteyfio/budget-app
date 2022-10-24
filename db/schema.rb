d7c02473d0e31990cd2ce1c3037cc07f4cee0b57
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-10-24 17:15:02 +0000

    Add `entities` scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_24_162138) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_24_165024) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "entities", force: :cascade do |t|
+    t.bigint "author_id", null: false
+    t.string "name"
+    t.decimal "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_entities_on_author_id"
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
@@ -29,5 +38,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_24_162138) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "entities", "users", column: "author_id"
   add_foreign_key "groups", "users"
 end
