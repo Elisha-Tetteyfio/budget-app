f1485ce8651aa2ff3a806e0b687b51cce9c8f5f2
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-10-27 10:27:56 +0000

    Add models tests

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  user = User.create(name: 'User1')
+  subject { described_class.new }
+  before :each do
+    subject.user = user
+    subject.name = 'Clothes'
+    subject.icon = "https//google.com"
+  end
+
+  it 'Expect the subject to be valid' do
+    expect(subject).to be_valid
+  end
+
+  it 'Should have a name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+  it 'Should have an icon' do
+    subject.icon = nil
+    expect(subject).to_not be_valid
+  end  
 end
+
