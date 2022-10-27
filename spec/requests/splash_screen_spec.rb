b33b2020079e494643b48693da13786930a0e0d4
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-10-27 11:10:18 +0000

    Add routing tests

--- spec/requests/splash_screen_spec.rb
+++ spec/requests/splash_screen_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "SplashScreens", type: :request do
   describe "GET /index" do
-    it "returns http success" do
-      get "/splash_screen/index"
-      expect(response).to have_http_status(:success)
+    it "it displays the Splash screen" do
+      get root_path
+      expect(response).to render_template(:index)
     end
   end
 
